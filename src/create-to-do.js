fcf79dcbd8c849a9e64f0f4b717adaa249755554
t0ntin <115113636+t0ntin@users.noreply.github.com>
2023-10-31 13:08:51 -0500

    Mostly done with modal styling. Working on app logic.

--- src/create-to-do.js
+++ src/create-to-do.js
@@ -2,11 +2,3 @@
 
 // title, description, due date, priority
 
-class Todo {
-  constructor(todo, dueDate, priority, description) {
-    this.todo = todo;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.description = description;
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import './style.css';
-import Icon from './hamburger.jpg';
+// import Icon from './hamburger.jpg';
 
-const title = document.querySelector('.title');
-const myIcon = new Image();
-myIcon.src = Icon;
+// const title = document.querySelector('.title');
+// const myIcon = new Image();
+// myIcon.src = Icon;
 // title.append(myIcon);
 
 
 
+class Todo {
+  constructor(todo, dueDate, priority, description) {
+    this.todo = todo;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.description = description;
+  }
+}
 
+function addTodo (todo, dueDate, priority, description) {
+  const todoObj = [];
+  const newToDo = new Todo (todo, dueDate, priority, description);
+  todoObj.push(newToDo);
+}
 
 console.log('testing');
