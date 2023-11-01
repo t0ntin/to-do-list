31388ea9710d13224ecd74eb8d035096a68df4d5
t0ntin <115113636+t0ntin@users.noreply.github.com>
2023-11-01 08:37:49 -0500

    Pushing data to main array successfully

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import './style.css';
 // title.append(myIcon);
 
 
+const mainTodoObj = [];
+
+
 
 class Todo {
   constructor(todo, dueDate, priority, description) {
@@ -18,9 +21,38 @@ class Todo {
 }
 
 function addTodo (todo, dueDate, priority, description) {
-  const todoObj = [];
   const newToDo = new Todo (todo, dueDate, priority, description);
-  todoObj.push(newToDo);
+  mainTodoObj.push(newToDo);
 }
 
+function handleSubmitToMain () {
+  let priority = 'testing';
+  const createTodoBtn = document.querySelector('#create-todo-btn');
+  createTodoBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const todo = document.querySelector('#to-do').value;
+    const dueDate = document.querySelector('#date').value;
+    const description = document.querySelector('#description').value;
+
+    addTodo(todo, dueDate, priority, description);
+    console.log(mainTodoObj);
+  });
+}
+
+handleSubmitToMain();
+
+
+
+//PREPOPULATES FIELDS
+document.addEventListener('DOMContentLoaded', function() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+
+  const formattedToday = `${yyyy}-${mm}-${dd}`;
+  document.getElementById('date').value = formattedToday;
+});
+
+console.log(mainTodoObj);
 console.log('testing');
