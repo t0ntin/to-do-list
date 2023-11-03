a6a558f90fbf25724702222ef9a75367586ddb0b
t0ntin <115113636+t0ntin@users.noreply.github.com>
2023-11-03 08:06:11 -0500

    Added delete section to todo item. All items resize properly

--- src/render-to-do.js
+++ src/render-to-do.js
@@ -4,15 +4,16 @@ import { mainTodoObj } from ".";
 
 
 export function renderTodos() {
-  const main = document.querySelector('.main');
-  main.innerHTML = '';
+  const todoContainer = document.querySelector('.todo-container');
+  todoContainer.innerHTML = '';
   for (const todo of mainTodoObj) {
-    main.innerHTML += `    
+    todoContainer.innerHTML += `    
     <ul class="todo-item">
     <li class="todo-title">${todo.todo}</li>
     <li class="todo-description">${todo.description}</li>
     <li class="todo-date">${todo.dueDate}</li>
     <li class="todo-priority">${todo.priority}</li>
+    <li class="todo-delete">Delete</li>
   </ul>`
 
   }
