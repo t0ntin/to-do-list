c1b3f7841efd50f4e406697dae24c11da88a68e6
t0ntin <115113636+t0ntin@users.noreply.github.com>
2023-11-25 11:13:44 -0500

    Added styles to modal.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
+
 import './style.css';
 import { prepopulateDate } from './logic';
 import { createPlusButton, submitToTodoContainer } from './dom.js';
-// import { isFirstDayOfMonth } from 'date-fns';
-
-// import Icon from './hamburger.jpg';
-
-// const title = document.querySelector('.title');
-// const myIcon = new Image();
-// myIcon.src = Icon;
-// title.append(myIcon);
-
-
-
-
-
-
-
 
 
 prepopulateDate();
 submitToTodoContainer();
 createPlusButton();
-// console.log(mainTodoArray);
-// console.log(projectArray);
+
